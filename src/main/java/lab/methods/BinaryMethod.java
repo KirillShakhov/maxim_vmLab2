8958f01ee3aed080a1342ec7fa5a5b355281e598
kirill <kirill.shakhov14@gmail.com>
2021-09-21 12:33:39 +0300

    fix table

--- src/main/java/lab/methods/BinaryMethod.java
+++ src/main/java/lab/methods/BinaryMethod.java
@@ -7,14 +7,14 @@ public class BinaryMethod {
     //Функция поиска корня: методом половинного деления
     public static Result solve(IFunc f, double a, double b, double eps){
         Result result = new Result();
-        result.addHeader("№", "a", "b", "x", "f(a)", "f(b)", "f(x)", "|a-b|");
+        result.addHeader("№", "  a  ", "   b  ", "   x  ", "   f(a)  ", "   f(b)  ", "   f(x)  ", "   |a-b|  ");
         double x = 0;
         int iter = 1;
-        while(Math.abs((Math.abs(b)-Math.abs(a))/2)>eps){
+        while(Math.abs(b-a)>eps){
             x = (a+b)/2;
             if((f.solve(a)*f.solve(x))>0) a=x;
             else b=x;
-            result.addCol(String.valueOf(a), String.valueOf(b), String.valueOf(x), f.solve(a).toString(), f.solve(b).toString(), f.solve(x).toString(), String.valueOf(Math.abs(b-a)));
+            result.addCol(String.format("%.5f",a), String.format("%.5f",b), String.format("%.5f",x), String.format("%.5f", f.solve(a)), String.format("%.5f",f.solve(b)), String.format("%.5f",f.solve(x)), String.format("%.5f",Math.abs(b-a)));
             iter++;
             if(iter > 100){
                 break;
